ad44438a4a3e6c8be3c7e79f46638c1f62fb2b3d
Jacob Pariseau <jacob@jacobpariseau.com>
2017-10-08 23:54:48 -0600

    Allowed access to Point

--- Source/components/Position.hx
+++ Source/components/Position.hx
@@ -3,29 +3,29 @@ package components;
 import util.Point;
 
 class Position {
-    private var position:Point;
+    public var point:Point;
 
     public function new(x:Float, y:Float) {
-        this.position = new Point(x, y);
+        this.point = new Point(x, y);
     }
 
     public var x(get, set):Int;
     public var y(get, set):Int;
 
     private inline function get_x():Int {
-        return this.position.x;
+        return this.point.x;
     }
 
     private inline function set_x(x) {
-        return this.position.x = x;
+        return this.point.x = x;
     }
 
     private inline function get_y():Int {
-        return this.position.y;
+        return this.point.y;
     }
 
     private inline function set_y(y) {
-        return this.position.y = y;
+        return this.point.y = y;
     }
     
 }
