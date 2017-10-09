42603009bdf1398093363518e8f033e3a5424c28
Jacob Pariseau <jacob@jacobpariseau.com>
2017-10-09 03:39:04 -0600

    Removed placeholder fields

--- Source/components/Building.hx
+++ Source/components/Building.hx
@@ -3,9 +3,7 @@ package components;
 import openfl.display.Tile;
 
 class Building {
-    public var tile:Tile;
     
-    public function new(tile:Tile) {
-        this.tile = tile;
+    public function new() {
     }
 }
