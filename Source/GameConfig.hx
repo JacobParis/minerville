ec3ab9157aca8393375fb50ecf62d65aa7620028
Jacob Pariseau <jacob@jacobpariseau.com>
2017-10-08 23:54:13 -0600

    Inlined constants

--- Source/GameConfig.hx
+++ Source/GameConfig.hx
@@ -1,9 +1,13 @@
 package;
 
 class GameConfig {
+    // Constants
+    public static inline var tileSize:Int = 48;
+    
+    // Variables
     public static var tilesLeft:Int = 0;
     public static var tilesUp:Int = 0;
     public static var tilesWide:Int = 10;
     public static var tilesHigh:Int = 10;
-    public static var tileSize:Int = 48;
+
 }
