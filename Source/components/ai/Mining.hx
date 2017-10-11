9c1f7161a21f6dc47b023c75042e4c038978c5d2
Jacob Pariseau <jacob@jacobpariseau.com>
2017-10-11 17:45:39 -0600

    Remove superfluous position field

--- Source/components/ai/Mining.hx
+++ Source/components/ai/Mining.hx
@@ -5,12 +5,10 @@ import ash.core.Entity;
 import util.Point;
 
 class Mining {
-    public var position:Point;
     public var block:Entity;
     public var strength:Int;
 
-    public function new(x:Float, y:Float, block:Entity, strength:Int = 1) {
-        this.position = new Point(x, y);
+    public function new(block:Entity, strength:Int = 1) {
         this.block = block;
         this.strength = strength;
     }
