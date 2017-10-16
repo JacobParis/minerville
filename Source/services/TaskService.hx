dae621b8bf7de7466edbfefa3279841cb0095eb8
Jacob Pariseau <jacob@jacobpariseau.com>
2017-10-15 21:31:14 -0600

    Simplified task sorting

--- Source/services/TaskService.hx
+++ Source/services/TaskService.hx
@@ -19,48 +19,29 @@ import util.ds.Stack;
 class TaskService {
     public static var instance(default, null):TaskService = new TaskService();
     public var queue:Queue<Task>;
-    public var stack:Stack<Task>;
     private function new() {
         this.queue = new ArrayedQueue();
-        this.stack = new ArrayedStack();
     }
     
     public function initialize():TaskService {
         return this;
     }
 	
-    public function addTask(task:Task, prioritize:Bool = false) {
-        if(prioritize) {
-            trace("Add priority task!");
-            for(s in this.stack) {
-                if(s.action == task.action
-                && s.target == task.target)
-                    this.stack.remove(s);
-            }
-            task.priority = 10;
-            this.stack.push(task);
-        } else {
-            for(q in this.queue) {
-                if(q.action == task.action
-                && q.target == task.target)
-                    return;
-            }
+    public function addTask(task:Task, priority:Float = 1) {
+        for(q in this.queue) {
+            if(q.action == task.action
+            && q.target == task.target) {
+                q.priority = priority;
+                return;
 
-            switch(task.action) {
-                case MINE: task.priority = 1;
-                case CARRY: task.priority = 2;
-                case ATTACK: task.priority = 3;
-                case WALK: task.priority = 0;
             }
-            this.queue.enqueue(task);
         }
+
+        task.priority = priority;
+        this.queue.enqueue(task);
     }
 
     public function getTask():Null<Task> {
-        if(this.stack.size > 0) {
-            return this.stack.pop();
-        }
-
         if(this.queue.size > 0) {
             return this.queue.dequeue();
         }
@@ -69,14 +50,6 @@ class TaskService {
     }
 
     public function removeTask(task:Task):Void {
-        for(s in this.stack) {
-            if(task.action == s.action
-            && task.target == s.target) {
-                this.stack.remove(s);
-                return;
-            }
-        }
-        
         for(q in this.queue) {
             if(q.action == task.action
             && q.target == task.target) {
@@ -87,25 +60,19 @@ class TaskService {
     }
 
 
-    public function getAllTasks():Array<Task> {
-        var tasks = new Array<Task>();
-        for(q in this.queue) {
-            tasks.push(q);
-        }
-        
-        for(s in this.stack) {
-            tasks.push(s);
-        }
-        tasks.reverse();
+    public function getAllTasks():Array<Task> {        
+        var tasks = queue.toArray();
+        tasks.sort(function (a:Task, b:Task) {
+            if (a.priority < b.priority) return 1;
+            else if (a.priority > b.priority) return -1;
+            return 0;
+        });
+
         return tasks;
     }
 
     public function hasTasks():Bool {
-        if(this.stack.size == 0 && this.queue.size == 0) {
-            return false;
-        }
-
-        return true;
+        return this.queue.size != 0;
     }
 }
 
